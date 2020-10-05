0a1c63ae7da82216b1f8a4b9058a859def711842
gabrielazanova <gabvzanova@gmail.com>
2020-10-05 13:02:47 +0200

    Menu

---
+++ Assets/LevelsMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelsMenu : MonoBehaviour
+{
+    public void LevelOne ()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LevelTwo()
+    {
+        Debug.Log("Level Two");
+    }
+
+    public void LevelThree()
+    {
+        Debug.Log("Level Three");
+    }
+    public void LevelFour()
+    {
+        Debug.Log("Level Four");
+    }
+}
