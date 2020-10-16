36d3a059a236259b2cf471c9dd2ef42d19c3fa1c
gabrielazanova <gabvzanova@gmail.com>
2020-10-16 18:05:38 +0200

    Survival Questions

---
+++ Assets/SurvivalQuestions.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SurvivalQuestions : MonoBehaviour
+{
+    [SerializeField] private SurvivalQuestionsUI survivalQuestionsUI;
+    [SerializeField] private SurvivalQuestionsDataScriptable survivalData;
+
+    private List<Question> questions;
+    private Question selectedQuestion;
+
+    private List<int> askedQuestions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        questions = survivalData.questions;
+
+        SelectQuestion();
+    }
+
+   void SelectQuestion()
+    {
+        if (questions.Count != 0)
+        {
+            int val = -1;
+            while (val < 0)
+            {
+                val = Random.Range(0, questions.Count);
+                bool hasAsked = false;
+                for (int i = 0; i < askedQuestions.Count; i++)
+                {
+                    if (askedQuestions[i] == val)
+                    {
+                        hasAsked = true;
+                    }
+                }
+                if (hasAsked == false)
+                {
+                    selectedQuestion = questions[val];
+                    askedQuestions.Add(val);
+
+                    survivalQuestionsUI.SetQuestion(selectedQuestion);
+                }
+            }
+        }
+        else
+        {
+            //SceneManager.LoadScene("Menu");
+        }
+    }
+
+    public bool Answer(string answered)
+    {
+        bool correctAns = false;
+
+        if (answered == selectedQuestion.correctAns)
+        {
+            //Yes
+            correctAns = true;
+        }
+        else
+        {
+            //No
+        }
+
+        //Remove question from the list so it cannot be picked again
+        //questions.Remove(selectedQuestion);
+
+        Invoke("SelectQuestion", 0.8f);
+
+        return correctAns;
+    }
+
+}
+
+[System.Serializable]
+public class Question
+{
+    public string questionInfo;
+    public List<string> options;
+    public string correctAns;
+}
+
+
---
+++ Assets/SurvivalQuestionsUI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SurvivalQuestionsUI : MonoBehaviour
+{
+
+    [SerializeField] private SurvivalQuestions survivalQuestions;
+    [SerializeField] private Text questionText;
+    [SerializeField] private List<Button> options;
+    [SerializeField] private Color correctCol, wrongCol, normalCol;
+
+    private Question question;
+    private bool answered;
+
+    void Awake()
+    {
+        for (int i = 0; i < options.Count; i++)
+        {
+            Button localBtn = options[i];
+            localBtn.onClick.AddListener(() => OnClick(localBtn));
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetQuestion(Question question)
+    {
+        this.question = question;
+
+        questionText.text = question.questionInfo;
+
+        //List<string> answerList = ShuffleList.ShuffleListItem<string>(question.options);
+
+        List<string> answerList = question.options;
+        for (int i = 0; i < options.Count; i++)
+        {
+            options[i].GetComponentInChildren<Text>().text = answerList[i];
+            options[i].name = answerList[i];
+            options[i].image.color = normalCol;
+        }
+
+        answered = false;
+    }
+
+    private void OnClick(Button btn)
+    {
+        if(!answered)
+        {
+            answered = true;
+            bool val = survivalQuestions.Answer(btn.name);
+
+            if(val)
+            {
+                btn.image.color = correctCol;
+            }
+            else
+            {
+                btn.image.color = wrongCol;
+            }
+        }
+    }
+
+}
