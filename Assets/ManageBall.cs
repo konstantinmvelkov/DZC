48f7ecb0ba0a8a6deda8ce5db661aa6e3c7deabf
Konstantin Velkov <konstantinmvelkov@gmail.com>
2020-10-16 18:04:17 +0200

    Agility levels

---
+++ Assets/ManageBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageBall : MonoBehaviour
+{
+    [SerializeField] Transform ballSpawnPoint;
+    
+    void OnCollisionEnter2D(Collision2D col) {
+        if(col.transform.CompareTag("Ball")) {
+            col.transform.position = ballSpawnPoint.position;
+        }
+
+        if(col.transform.CompareTag("BigFlake")) {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/deployBalls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployBalls : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float respawnTime = 1.0f;
+    [SerializeField] Transform spawnPoint;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(ballWave());
+    }
+    private void spawnBall(){
+        GameObject ball = Instantiate(ballPrefab) as GameObject;
+        ball.transform.position =  spawnPoint.position;
+    }
+    IEnumerator ballWave(){
+        while(true){
+            yield return new WaitForSeconds(respawnTime);
+            spawnBall();
+        }
+    }
+}
