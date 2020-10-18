acb3a867112fc13b08d5b958b198f688840e41ea
Bram Haverkort <b.f.haverkort@student.tue.nl>
2020-10-18 19:30:18 +0200

    Improvement on fireball level and start of rising lava level

---
+++ Assets/SurvivalTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SurvivalTimer : MonoBehaviour
+{
+    public Text timerText;
+
+    private string text = "Survive!\nTime left: ";
+
+    public float time = 100;
+
+    public int timeOutSceneBuildIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        timerText.text = text + time.ToString("0.0") + "s";
+
+        if(time <= 0)
+        {
+            SceneManager.LoadScene(timeOutSceneBuildIndex);
+        }
+    }
+}
