a1062248ef6b8f84f1004af32acbdbd5ffebc64c
konstantinmvelkov <konstantinmvelkov@gmail.com>
2020-10-22 15:49:50 +0200

    Added background music and die sound effect

---
+++ Assets/DontDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("awake DontDestroy");
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
