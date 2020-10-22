8a2268f0fcb9005e87f5c4a46da28787f63c4159
gabrielazanova <gabvzanova@gmail.com>
2020-10-22 15:51:23 +0200

    2 New SurvivalQuestions Levels

---
+++ Assets/SurvivalQuestionsDataScriptable2.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestionData2", menuName = "QuestionData2")]
+public class SurvivalQuestionsDataScriptable2 : ScriptableObject
+{
+    public List<Question> questions;
+}
